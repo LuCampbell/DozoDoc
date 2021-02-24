1d9f69f289f5a7790886a83a0d34a8c9cd9a1a5b
Yann <yann.lukowiak@epsi.fr>
2021-02-24 15:28:33 +0100

    Add insegel theme

--- source/conf.py
+++ source/conf.py
@@ -10,8 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+sys.path.append(os.path.join(os.environ['SAMPLE_DOCS_LOCATION'], 'demo'))
+print('', sys.path[-1], '', sep='\n' + '-' * 80 + '\n')
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -31,6 +33,10 @@ release = '1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,7 +60,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'insegel'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
