2e52b7a04b7f7fd6a28618d04fe1fd9e5a9105b1
Yann <yann.lukowiak@epsi.fr>
2021-02-24 15:30:59 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -12,9 +12,7 @@
 #
 import os
 import sys
-sys.path.append(os.path.join(os.environ['SAMPLE_DOCS_LOCATION'], 'demo'))
-print('', sys.path[-1], '', sep='\n' + '-' * 80 + '\n')
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
